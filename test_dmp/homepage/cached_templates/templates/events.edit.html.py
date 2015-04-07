5260320c2bb0c9e4874b7f0c8b3dd1559637075f
spencerwsmith <spencerw.smith@yahoo.com>
2015-04-07 13:34:44 -0600

    4/7/15

--- test_dmp/homepage/cached_templates/templates/events.edit.html.py
+++ test_dmp/homepage/cached_templates/templates/events.edit.html.py
@@ -4,9 +4,9 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1423123009.151914
+_modified_time = 1428433388.264691
 _enable_loop = True
-_template_filename = 'C:\\Python34\\Lib\\site-packages\\django\\bin\\test_dmp\\homepage\\templates/events.edit.html'
+_template_filename = 'C:\\Users\\Spencer\\Documents\\School\\CHF\\chf\\test_dmp\\homepage\\templates/events.edit.html'
 _template_uri = 'events.edit.html'
 _source_encoding = 'ascii'
 import os, os.path, re
@@ -28,10 +28,10 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        form = context.get('form', UNDEFINED)
-        event = context.get('event', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        form = context.get('form', UNDEFINED)
+        event = context.get('event', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
@@ -46,14 +46,14 @@ def render_body(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        form = context.get('form', UNDEFINED)
-        event = context.get('event', UNDEFINED)
         def content():
             return render_content(context)
+        form = context.get('form', UNDEFINED)
+        event = context.get('event', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n  <form method="POST">\r\n   <table>\r\n    ')
         __M_writer(str( form ))
-        __M_writer('\r\n   </table>\r\n    <button type="submit" class="\'btn btn-primary">Submit</button>\r\n    <a href ="/homepage/events.delete/')
+        __M_writer('\r\n   </table>\r\n    <button type="submit" class="btn btn-primary">Submit</button>\r\n    <a href ="/homepage/events.delete/')
         __M_writer(str( event.id ))
         __M_writer('/" class="btn btn-danger">Delete Event</a>\r\n\r\n  </form>\r\n\r\n')
         return ''
@@ -63,6 +63,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:\\Python34\\Lib\\site-packages\\django\\bin\\test_dmp\\homepage\\templates/events.edit.html", "source_encoding": "ascii", "line_map": {"64": 58, "36": 1, "54": 3, "55": 7, "56": 7, "57": 10, "58": 10, "27": 0, "46": 3}, "uri": "events.edit.html"}
+{"line_map": {"64": 58, "36": 1, "54": 3, "55": 7, "56": 7, "57": 10, "58": 10, "27": 0, "46": 3}, "uri": "events.edit.html", "source_encoding": "ascii", "filename": "C:\\Users\\Spencer\\Documents\\School\\CHF\\chf\\test_dmp\\homepage\\templates/events.edit.html"}
 __M_END_METADATA
 """
